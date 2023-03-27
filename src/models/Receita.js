a90baf75b8e5ba2ed3f49633686730ab6f20d4b6
Klaus7067 <riansouza589@gmail.com>
2023-03-26 23:15:12 -0300

    já deu

--- src/models/Receita.js
+++
@@ -1,9 +0,0 @@
-class Receita{
-    constructor(titulo, descricao, modo_preparo,tempo_preparo){
-        this.titulo=titulo
-        this.descricao=descricao
-        this.modo_preparo=modo_preparo
-        this.tempo_preparo=tempo_preparo
-    }
-}
-export default Receita
